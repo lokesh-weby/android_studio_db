b25bca31864a6fea3d21ed7b242ad6c7ec5e3e1d
[lokesh-weby] <lokesh0212004@gmail.com>
2025-05-20 18:04:26 +0530

    shorten program

--- app/src/main/java/com/example/db_connectivity/MainActivity.java
+++ app/src/main/java/com/example/db_connectivity/MainActivity.java
@@ -1,33 +1,23 @@
 package com.example.db_connectivity;
-
 import android.app.AlertDialog.Builder;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
 import android.view.View.OnClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
     EditText RollNo,Name,Percentage;
-    Button Insert,Delete,Update,View,ViewAll;
+    Button Insert,Delete,Update,ViewAll;
     SQLiteDatabase db;
-    public final String CHANNEL_ID = "my_channel_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         db=openOrCreateDatabase("studentDB",Context.MODE_PRIVATE,null);
         db.execSQL("CREATE TABLE IF NOT EXISTS students (rollno VARCHAR NOT NULL,name VARCHAR,marks VARCHAR);");
@@ -38,11 +28,9 @@ public class MainActivity extends AppCompatActivity {
         RollNo=findViewById(R.id.rollNo);
         Name=findViewById(R.id.Name);
         Percentage=findViewById(R.id.Percentage);
-
         Insert=findViewById(R.id.insert);
         Delete=findViewById(R.id.delete);
         Update=findViewById(R.id.update);
-        View=findViewById(R.id.view);
         ViewAll=findViewById(R.id.ViewAll);
 
         Insert.setOnClickListener(new OnClickListener() {
@@ -56,25 +44,8 @@ public class MainActivity extends AppCompatActivity {
                     db.execSQL("insert into students values('"+RollNo.getText()+"','"+Name.getText()+"','"+Percentage.getText()+"');");
                     showMessage("Success","Recode added");
                     clearText();
-                    createNotificationChannel();
-                    showNotification("StudentDB","Successfully data inserted!!");
                 }
 
-            }
-        });
-
-        View.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                Cursor c=db.rawQuery("SELECT * FROM students WHERE rollno='" + RollNo.getText() + "'", null);
-                if(c.moveToFirst()){
-                    Name.setText(c.getString(1));
-                    Percentage.setText(c.getString(2));
-                }
-                else {
-                    showMessage("Error","Invalid RollNo");
-                    clearText();
-                }
             }
         });
 
@@ -103,13 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(android.view.View v) {
-
-
-
                 db.execSQL("DELETE FROM students WHERE rollno='" + RollNo.getText() + "'");
                 showMessage("Deleted Successfully","RollNo "+RollNo.getText());
                 clearText();
-
             }
         });
 
@@ -125,45 +92,10 @@ public class MainActivity extends AppCompatActivity {
                 showMessage("Updated ","Successfully Updated");
                 clearText();
                 }
-
-
             }
         });
 
     }
-
-
-    public void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "My Channel";
-            String description = "Channel for test notification";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "My Channel",
-                    NotificationManager.IMPORTANCE_HIGH
-            );
-            channel.enableLights(true);
-            channel.setLightColor(Color.BLUE);
-            channel.setDescription(description);
-
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel);
-        }
-    }
-
-    public void showNotification(String title,String content) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(title)
-                .setContentText(content)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setLights(Color.BLUE,1000,1000);
-
-        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-        notificationManager.notify(1, builder.build());
-    }
-
     public boolean validate(String rollNo,String name, String percentage){
         if(rollNo.isEmpty() || name.isEmpty() || percentage.isEmpty())
         {
@@ -183,8 +115,5 @@ public class MainActivity extends AppCompatActivity {
         Builder builder=new Builder(this);
         builder.setCancelable(true); builder.setTitle(title);
         builder.setMessage(msg); builder.show();
-
     }
-
-
 }
